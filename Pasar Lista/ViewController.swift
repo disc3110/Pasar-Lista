29d935a043f1cdd3b8de583d25e84f7c3c4128d7
disc3110 <pargentin007@comunidad.unam.mx>
2019-10-25 14:02:56 -0500

    Initial Commit

---
+++ Pasar Lista/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pasar Lista
+//
+//  Created by Diego Ivan Solis Cuevas on 25/10/19.
+//  Copyright © 2019 Diego Ivan Solis Cuevas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
